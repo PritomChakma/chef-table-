ad937fba39a095f22343660c1988cbd90b419e6e
pritom <chakmapritom1@gmail.com>
2024-10-23 21:20:37 +0600

    setup all file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Banner from "./Components/Banner";
 import Header from "./Components/Header";
 import OurRecepie from "./Components/OurRecepie";
+import Recepie from "./Components/Recepie";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
@@ -10,7 +12,14 @@ function App() {
       {/* Banner */}
       <Banner></Banner>
       {/* Our Recepie section */}
-      <OurRecepie></OurRecepie>
+      <OurRecepie>
+        <section>
+          {/* Cart section */}
+          <Recepie></Recepie>
+          {/* sidebar Section */}
+          <Sidebar></Sidebar>
+        </section>
+      </OurRecepie>
     </div>
   );
 }
---
+++ src/Components/Recepie.jsx
@@ -0,0 +1,9 @@
+const Recepie = () => {
+  return (
+    <div>
+      <h1>REcepiee section</h1>
+    </div>
+  );
+};
+
+export default Recepie;
